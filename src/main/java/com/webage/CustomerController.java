58b5e6d98c5409871245f778fe71d2944605be60
wasadmin <kelsey.marantan@gmail.com>
2024-08-20 18:50:05 +0000

    add customer method

--- src/main/java/com/webage/CustomerController.java
+++ src/main/java/com/webage/CustomerController.java
@@ -1,13 +1,18 @@
 package com.webage;
 
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webage.repository.CustomersRepository;
 
@@ -34,5 +39,26 @@ public class CustomerController{
 	@GetMapping("/customers/{customerId}")
 	public Optional <Customer>getCustomerById(@PathVariable("customerId") long id){
 		return repo.findById(id);
+	}
+	
+	// Create customer
+	@PostMapping("/customers")
+	public ResponseEntity<?> addCustomer(@RequestBody Customer newCustomer){
+		// Validate Input
+		if(newCustomer.getName()==null 
+				|| newCustomer.getEmail()==null) {
+			return ResponseEntity.badRequest().build();
+		}
+		newCustomer = repo.save(newCustomer);
+		
+		URI location = 
+				ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{customerId}")
+				.buildAndExpand(newCustomer.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();
+		
 	}
 }
