6d74a78ffdb000ad0d3e18d86d9cf82727713f67
wasadmin <kelsey.marantan@gmail.com>
2024-08-20 15:18:37 +0000

    added test default for /api endpoint

--- src/test/java/com/webage/StudentApplicationTests.java
+++ src/test/java/com/webage/StudentApplicationTests.java
@@ -1,5 +1,8 @@
 package com.webage;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +14,15 @@ class StudentApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void testDefault() {
+		var controller = new CustomerController();
+		String result = controller.defaultAPI();
+		
+		assertNotNull(result);
+		assertTrue(result.contains("API is working"));
+	}
+	
 	@Test
 	@Disabled
 	public void testGetAll()
