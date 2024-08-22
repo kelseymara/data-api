31228319c32fb5995244021e1fbdd4d37c67c6c6
Kelsey Marantan <kelsey.marantan@gmail.com>
2024-08-22 15:08:08 -0400

    Added 401 failure test

--- src/test/java/com/webage/CustomerAPITests.java
+++ src/test/java/com/webage/CustomerAPITests.java
@@ -1,5 +1,7 @@
 package com.webage;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,8 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 
-import com.webage.domain.Customer;
 
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class CustomerAPITests{
@@ -21,60 +25,19 @@ public class CustomerAPITests{
 	@Autowired TestRestTemplate template;
 	
 	@Test
-	@Disabled
-	public void testGetList() {
-		Customer[] customers = template.getForObject("/api/customers", Customer[].class);
-		
-		assertNotNull(customers);
-		assertNotNull(customers[0]);
-		assertNotNull(customers[0].getId());
-		assertTrue(customers.length>0);
-		
-	}
-	
-	@Test
-	@Disabled
-	public void testGet() {
-		Customer customer = template.getForObject("/api/customers/{customerId}", Customer.class,1);
-		
-		assertNotNull(customer);
-		assertNotNull(customer.getId());
-	}
-	
-	@Test
-	@Disabled
-	public void testCreate() {
-		Customer customer = new Customer();
-		customer.setName("Test");
-		customer.setEmail("test@mail.com");
-		
-		URI location = template.postForLocation("/api/customers", customer,Customer.class);
-		assertNotNull(location);
-		
-		customer= template.getForObject(location, Customer.class);
-		assertNotNull(customer);
-		assertNotNull(customer.getId());
-		assertEquals("Test", customer.getName());
-		assertEquals("test@mail.com",customer.getEmail());
-	}
-	
-	@Test
-	@Disabled
-	public void testPut()
-	{
-		String path = "/api/customers/5";
-		String newValue = "NewValue";
-		
-		Customer customer = template.getForObject(path, Customer.class);
-		
-		customer.setName(newValue);
-		template.put(path, customer);
-		
-		customer = template.getForObject(path, Customer.class);
-		
-		assertEquals(newValue, customer.getName());
-		
-		
-	}
+   // @Disabled
+    void rootWhenUnauthenticatedThen401() throws Exception {
+        //  GET the root resource without passing in any Authentication.
+        //  We expect a 401 unauthorized failure. 
+        RequestEntity request = RequestEntity
+            .get("/api/customers")
+            .build();
+
+        ResponseEntity<String> responseEntity =
+            template.exchange(request,String.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
 	
 }
