fd22a0912d84fa1e90187a58cc5acdaaa0bcf5a0
Kelsey Marantan <kelsey.marantan@gmail.com>
2024-09-11 13:10:11 -0400

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-insert INTO CUSTOMERS (CUSTOMER_NAME,EMAIL,PASSWORD) values ('kelsey','kelsey@mail.com','pass123');
-insert INTO CUSTOMERS (CUSTOMER_NAME,EMAIL,PASSWORD) values ('pom','pom@mail.com','pass456');
+insert INTO CUSTOMERS (CUSTOMER_NAME,EMAIL,PASSWORD) values ('Kelsey','kelsey@mail.com','pass123');
+insert INTO CUSTOMERS (CUSTOMER_NAME,EMAIL,PASSWORD) values ('Melanie','melanie@mail.com','pass456');
+insert INTO CUSTOMERS (CUSTOMER_NAME,EMAIL,PASSWORD) values ('Tuan','tuan@mail.com','pass789');
